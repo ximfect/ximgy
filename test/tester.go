55b89735fa203c10fadf95319b2c8bf8792ad32e
qeambuntu <qeambuntu@protonmail.com>
2020-08-19 23:59:39 +0200

    update test

--- test/tester.go
+++ test/tester.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 
 	"github.com/ximfect/ximgy"
 )
+
+func invert(pixel ximgy.Pixel) color.RGBA {
+	return color.RGBA{255 - pixel.R, 255 - pixel.G, 255 - pixel.B, pixel.A}
+}
 
 func main() {
 	img, err := ximgy.Open("EffzbrUXsAMKkkp.png")
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println("image loaded", img.Size)
+		return
 	}
+
+	fmt.Println("image loaded", img.Size)
+	img.Iterate(invert)
+	fmt.Println("image iterated")
+
+	err = ximgy.Save(img, "out.png")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("image saved")
 }
